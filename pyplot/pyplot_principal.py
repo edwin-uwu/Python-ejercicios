c045450ec7c67413aa1fe246393ff355c1f68e9f
Edwin Hdz <edwinhotmail663@gmail.com>
2025-11-04 23:45:26 -0600

    Personalización de gráficas

--- pyplot/pyplot_principal.py
+++ pyplot/pyplot_principal.py
@@ -1,9 +1,19 @@
+from cProfile import label
 import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.arange(0,10,0.1);
 y= np.sin(x);
 z = np.cos(x);
-plt.plot(x,y);
-plt.plot(x,z);
+plt.plot(x,y,label='sin(x)');
+plt.plot(x,z,label='cos(x)');
+#Personalización de gráficos
+print('--------------------')
+plt.grid();#Regillas en el gráfico
+plt.title('Mi primer gráfico');#Título
+plt.legend();#Mostrar leyenda
+plt.xlabel('Eje X');#Título eje x
+plt.ylabel('Eje Y');#Título eje y
+plt.xlim(0,4);#Límites eje x
+
 plt.show();
