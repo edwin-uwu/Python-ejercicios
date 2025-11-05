8862659c76cfeab57e2a92bcb13c350e7f4f8413
Edwin Hdz <edwinhotmail663@gmail.com>
2025-11-05 13:22:30 -0600

    Gráficas distintas

--- pyplot/pyplot_principal.py
+++ pyplot/pyplot_principal.py
@@ -2,18 +2,40 @@ from cProfile import label
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.arange(0,10,0.1);
-y= np.sin(x);
-z = np.cos(x);
-plt.plot(x,y,label='sin(x)');
-plt.plot(x,z,label='cos(x)');
+#x = np.arange(0,10,0.1);
+#y= np.sin(x);
+#z = np.cos(x);
+##plt.plot(x,y,label='sin(x)');
+##plt.plot(x,z,label='cos(x)');
 #Personalización de gráficos
 print('--------------------')
-plt.grid();#Regillas en el gráfico
-plt.title('Mi primer gráfico');#Título
-plt.legend();#Mostrar leyenda
-plt.xlabel('Eje X');#Título eje x
-plt.ylabel('Eje Y');#Título eje y
-plt.xlim(0,4);#Límites eje x
+#plt.grid();#Regillas en el gráfico
+#plt.title('Mi primer gráfico');#Título
+#plt.legend();#Mostrar leyenda
+#plt.xlabel('Eje X');#Título eje x
+#plt.ylabel('Eje Y');#Título eje y
+#plt.xlim(0,4);#Límites eje x
 
+#plt.show();
+#Tipo de gráficas
+x = np.random.random([50]);
+y = np.random.random([50]);
+print(x);
+print(y);
+#Scatter plot grafica por puntos
+#plt.scatter(x,y)
+
+#Bar plot gráfica de barras
+opciones=['si','no'];
+resultados=[40,50];
+#plt.bar(opciones,resultados);
+
+#Histogramas
+#plt.hist(x,15);
+
+#Box plot
+#plt.boxplot(x);
+
+#pie plot
+plt.pie(resultados,labels=opciones);
 plt.show();
