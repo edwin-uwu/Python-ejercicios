93ae05cae633243698c2dc22c75971b9c0b06485
Edwin Hdz <edwinhotmail663@gmail.com>
2025-11-19 00:30:09 -0600

    Datos

--- pandas/pandas_importardatos.py
+++ pandas/pandas_importardatos.py
@@ -11,4 +11,17 @@ df1 = pd.read_excel('figurasanime.xlsx');
 #print(df1.head(2))
 #print(df1.sample(12))
 #print(df1.describe())
-print(df1.info())
+#print(df1.info())
+#Accediendo datos de dataframe
+
+df3 = pd.read_excel('figurasanime.xlsx');
+df3.set_index('nombre',inplace=True);
+#print(df3.head());
+
+print(df3['anime'].head(10)); # obtener columnas
+print(df3.iloc[7]);#Obtener datos por filas
+
+#Rangos y filtrado de datos
+
+print(df3.iloc[37:]);
+print("\n",df3[(df3['distance'] > 6) & (df3['climb']> 5)])
